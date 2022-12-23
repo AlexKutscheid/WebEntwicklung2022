e444d71a3b04e0eab18c3ec95e2a308c95c39f23
alex <alexanderkutscheid96@t-online.de>
2022-12-23 15:29:51 +0100

    codeigniter eingebunden

---
+++ ci/app/Controllers/Home.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+    public function index()
+    {
+        // $data['title'] = "Unser erster Title";
+        // echo view ('templates/header');
+        // echo view ('pages/simple', $data);
+        // echo view ('templates/footer');
+        return view('welcome_message');
+    }
+}
